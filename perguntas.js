8e2f6b54f7c2427ccbf302ae953d360f120aa2e7
Nathan <nathan.wolker@escola.pr.gov.br>
2025-06-26 16:09:48 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+'Geografia',
+'Qual país fica Washington?',
+'O país que fica Washington é Estados Unidos'
+)
+
+criaCartao(
+'História',
+'Quem descobriu o Brasil?',
+'Quem descobriu o Brasil foi Pedro Álvares Cabral'
+)
+
+criaCartao(
+'Esportes',
+'Quantas Copas do Mundo o Brasil tem?',
+'O Brasil atualmente é Hexacampeão, ou seja, cinco vitórias'
+)
+
+criaCartao(
+'Religião',
+'Qual religião tem mais fiéis no Brasil?',
+'Cristianismo'
+)
+
+criaCartao(
+'Matemática',
+'Quanto é 2 + 2?',
+'2 + 2 = 4'
+)
+
+criaCartao(
+'História',
+'Hitler foi bom?',
+'Não, devido a ditadura que ele implantou, e o genocidio de 6 milhões de Judeus.'
+)
